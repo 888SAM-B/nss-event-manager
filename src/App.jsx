71809857cae2b71ee1469ff85607904c1b7e9086
888SAM-B <bsam53888@gmail.com>
2026-01-13 21:20:34 +0530

    frontend added

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Home from './components/home'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <div className="home">
+        <Home />
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/home.jsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react";
+
+const Home = () => {
+    return (
+        <>
+        <h1>Lets Start</h1>
+        </>
+    );
+};
+
+export default Home;
