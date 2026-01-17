f14dcb19d7cbbf7e5a688cae8f80a772d818b10e
888SAM-B <bsam53888@gmail.com>
2026-01-17 12:09:06 +0530

    login

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
-
+const jwt = require('jsonwebtoken');
+const cors = require("cors");
 const app = express();
+app.use(cors());
+app.use(express.json());
 
 const PORT = process.env.PORT;
 const mongoURI = process.env.MONGODB_URI;
@@ -11,8 +14,42 @@ mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Connected to MongoDB'))
     .catch(err => console.error('Failed to connect to MongoDB', err));
 
+const insLoginScheme = new mongoose.Schema({
+    userName: String,
+    password: String
+});
+
+const User = mongoose.model('User', insLoginScheme);
+
 app.get('/', (_req, res) => {
     res.send('Hello, World!');
+});
+
+app.post('/login', async (req, res) => {
+    console.log('inside login');
+
+    const { username, password } = req.body;
+    console.log(username, password);
+    console.log(await User.find())
+    const user = await User.findOne({ userName: username, password });
+
+    if (!user) {
+        return res.status(401).json({
+            success: false,
+            message: "Invalid username or password"
+        });
+    }
+
+    const token = jwt.sign(
+        { userName: username },
+        process.env.JWT_SECRET,
+        { expiresIn: "1d" }
+    );
+
+    res.json({
+        success: true,
+        token
+    });
 });
 
 app.listen(PORT, () => {
